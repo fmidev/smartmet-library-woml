90a1cbc029792ccde43a12d7f7da5bf6e3dac7c0
mheiskan <mheiskan>
2010-06-02 06:28:05 +0000

    Test drivers for the remaining weather objects.

--- test/WomlTest.cpp
+++ test/WomlTest.cpp
@@ -58,6 +58,72 @@ namespace WomlTest
 	TEST_PASSED();
   }
 
+  void jet()
+  {
+	woml::Weather weather = woml::parse(inputdir / "jet.xml");
+	TEST_PASSED();
+  }
+
+  void occlusion()
+  {
+	woml::Weather weather = woml::parse(inputdir / "occlusion.xml");
+	TEST_PASSED();
+  }
+
+  void trough()
+  {
+	woml::Weather weather = woml::parse(inputdir / "trough.xml");
+	TEST_PASSED();
+  }
+
+  void uppertrough()
+  {
+	woml::Weather weather = woml::parse(inputdir / "uppertrough.xml");
+	TEST_PASSED();
+  }
+
+  void rain()
+  {
+	woml::Weather weather = woml::parse(inputdir / "rain.xml");
+	TEST_PASSED();
+  }
+
+  void heavyrain()
+  {
+	woml::Weather weather = woml::parse(inputdir / "heavyrain.xml");
+	TEST_PASSED();
+  }
+
+  void snow()
+  {
+	woml::Weather weather = woml::parse(inputdir / "snow.xml");
+	TEST_PASSED();
+  }
+
+  void value()
+  {
+	woml::Weather weather = woml::parse(inputdir / "value.xml");
+	TEST_PASSED();
+  }
+
+  void values()
+  {
+	woml::Weather weather = woml::parse(inputdir / "values.xml");
+	TEST_PASSED();
+  }
+
+  void pointnote()
+  {
+	woml::Weather weather = woml::parse(inputdir / "pointnote.xml");
+	TEST_PASSED();
+  }
+
+  void graphic()
+  {
+	woml::Weather weather = woml::parse(inputdir / "graphic.xml");
+	TEST_PASSED();
+  }
+
   //! The actual test driver
   class tests : public tframe::tests
   {
@@ -73,10 +139,21 @@ namespace WomlTest
 	  TEST(empty);
 	  TEST(warmfront);
 	  TEST(coldfront);
+	  TEST(occlusion);
 	  TEST(coldandwarmfront);
+	  TEST(jet);
 	  TEST(warmfronts);
 	  TEST(signs);
 	  TEST(cloud);
+	  TEST(trough);
+	  TEST(uppertrough);
+	  TEST(snow);
+	  TEST(rain);
+	  TEST(heavyrain);
+	  TEST(value);
+	  TEST(values);
+	  TEST(pointnote);
+	  TEST(graphic);
 	}
 
   }; // class tests
