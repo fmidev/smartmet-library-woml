b87d1757afb668215941a392e146d43c22027a91
Mika Heiskanen <mika.heiskanen@fmi.fi>
2011-08-30 12:02:23 +0300

    Fixed function signature to match that in base class.

--- test/WomlTest.cpp
+++ test/WomlTest.cpp
@@ -134,7 +134,7 @@ namespace WomlTest
   class tests : public tframe::tests
   {
 	//! Overridden message separator
-	virtual const char * const error_message_prefix() const
+	virtual const char * error_message_prefix() const
 	{
 	  return "\n\t";
 	}
